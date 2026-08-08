587e156fc02b21521cafa8929a4602484c90cd7f
KUMAR <cjvkumarraja@gmail.com>
2026-08-08 21:19:54 +0530

    Time: 1 ms (100.00%) | Memory: 49.3 MB (68.96%) - LeetSync

--- LinkedList/1765-merge-in-between-linked-lists/merge-in-between-linked-lists.java
+++ LinkedList/1765-merge-in-between-linked-lists/merge-in-between-linked-lists.java
@@ -14,14 +14,17 @@ class Solution {
         while(tail2.next!=null){
             tail2=tail2.next;
         }
-        ListNode temp = list1;
-        ListNode start=null;
-        ListNode end= null;
-        for(int i =0;i<=b+1;i++){
-            if(i==a-1) start = temp ;
-            if(i==b+1) end=temp;
-            temp= temp.next;
-        }
+        // ListNode temp = list1;
+        ListNode start=list1;;
+        // for(int i =0;i<=b+1;i++){
+        //     if(i==a-1) start = temp ;
+        //     if(i==b+1) end=temp;
+        //     temp= temp.next;
+        // }
+        for(int i = 0;i<a-1;i++) start=start.next;
+        ListNode end= start.next;
+        for(int i=a;i<b+1;i++) end= end.next;
+
         start.next = list2;
         tail2.next=end;
         return list1;
