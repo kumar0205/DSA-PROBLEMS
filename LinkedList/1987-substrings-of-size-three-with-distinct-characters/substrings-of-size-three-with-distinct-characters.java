789cac7c2b87f01ff2e87325a0e002eeeebcab49
KUMAR <cjvkumarraja@gmail.com>
2026-08-14 19:57:01 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (44.61%) - LeetSync

--- LinkedList/1987-substrings-of-size-three-with-distinct-characters/substrings-of-size-three-with-distinct-characters.java
+++ LinkedList/1987-substrings-of-size-three-with-distinct-characters/substrings-of-size-three-with-distinct-characters.java
@@ -1,18 +1,36 @@
+// class Solution {
+//     public int countGoodSubstrings(String s) {
+//         int r=2,c=0;
+//         // if(s.charAt(0)==s.charAt(1)){
+//         //     r=3;
+//         //     // l=1;
+//         // }
+//         while(r<s.length()){
+//             if(s.charAt(r)!=s.charAt(r-1) && s.charAt(r-1)!=s.charAt(r-2) && s.charAt(r)!=s.charAt(r-2)){
+//                      c++;
+                
+//             }
+//             // l++;
+//             r++;
+//         }
+//         return c;
+//     }
+// }
 class Solution {
     public int countGoodSubstrings(String s) {
-        int r=2,c=0;
-        // if(s.charAt(0)==s.charAt(1)){
-        //     r=3;
-        //     // l=1;
-        // }
-        while(r<s.length()){
-            if(s.charAt(r)!=s.charAt(r-1) && s.charAt(r-1)!=s.charAt(r-2) && s.charAt(r)!=s.charAt(r-2)){
-                     c++;
-                
+
+        char[] str = s.toCharArray();
+        int count = 0;
+
+        for (int i = 0; i < str.length - 2; i++) {
+
+            if (str[i] != str[i + 1]
+                    && str[i] != str[i + 2]
+                    && str[i + 1] != str[i + 2]) {
+                count++;
             }
-            // l++;
-            r++;
         }
-        return c;
+
+        return count;
     }
 }
