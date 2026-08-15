a02a813f9b03c906fbd1f6832cc4cc6bcddd6ea4
KUMAR <cjvkumarraja@gmail.com>
2026-08-15 09:06:20 +0530

    Time: 28 ms (96.83%) | Memory: 95.3 MB (93.78%) - LeetSync

---
+++ LinkedList/2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.java
@@ -0,0 +1,38 @@
+import java.util.HashMap;
+
+class Solution {
+    public long maximumSubarraySum(int[] nums, int k) {
+        // Bug Fix 1: Change sum and maxsum to long to prevent integer overflow
+        int l = 0, r = 0, n = nums.length;
+        long sum = 0, maxsum = 0; 
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        
+        while (r < n) {
+            sum += nums[r];
+            
+            if (!hm.containsKey(nums[r])) {
+                hm.put(nums[r], 1);
+                if (r - l + 1 == k) {
+                    maxsum = Math.max(maxsum, sum);
+                    sum -= nums[l];
+                    hm.remove(nums[l]);
+                    l++;
+                }
+            }
+            else {
+                // Bug Fix 2: Only shrink until the previous instance of nums[r] is removed
+                while (nums[l] != nums[r]) {
+                    sum -= nums[l];
+                    hm.remove(nums[l]);
+                    l++;
+                }
+                // Subtract the matching duplicate element itself from the sum
+                sum -= nums[l];
+                // Do NOT remove it from the map, because nums[r] is about to take its place!
+                l++; 
+            }
+            r++;
+        }
+        return maxsum;
+    }
+}
