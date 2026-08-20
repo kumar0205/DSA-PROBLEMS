ee8e0d6b14904bcf11f807601ac25fd4eacd7033
KUMAR <cjvkumarraja@gmail.com>
2026-08-20 21:58:40 +0530

    Time: 30 ms (32.03%) | Memory: 94.5 MB (59.57%) - LeetSync

---
+++ Arrays/PrefixSum/2471-minimum-amount-of-time-to-collect-garbage/minimum-amount-of-time-to-collect-garbage.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int garbageCollection(String[] garbage, int[] travel) {
+        int mi = 0, mf = 0, gi = 0, gf = 0, pi = 0, pf = 0, sum = 0, n = garbage.length;
+        
+        // 1. Prefix sum remains the same
+        for(int i = 1; i < travel.length; i++) {
+            travel[i] += travel[i - 1];
+        }
+        
+        // 2. Finding frequencies and last indices
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < garbage[i].length(); j++) {
+                if(garbage[i].charAt(j) == 'M') {
+                    mf++;
+                    mi = i;
+                }
+                else if(garbage[i].charAt(j) == 'G') {
+                    gf++;
+                    gi = i;
+                }
+                // FIX 1: Changed lowercase 'p' to uppercase 'P'
+                else if(garbage[i].charAt(j) == 'P') {
+                    pf++;
+                    pi = i;
+                }
+            }
+        }
+        
+        // FIX 2: Total picking time is just the sum of all frequencies
+        sum = mf + gf + pf;
+        
+        // FIX 3: Removed the "else if" chain so all trucks get calculated.
+        // Added "> 0" checks to prevent ArrayIndexOutOfBoundsException (travel[-1]).
+        if(mi > 0) sum += travel[mi - 1];
+        if(gi > 0) sum += travel[gi - 1];
+        if(pi > 0) sum += travel[pi - 1];
+        
+        return sum;
+    }
+}
