7c85b865f715922403a0bed9e071a6f440ddf6df
KUMAR <cjvkumarraja@gmail.com>
2026-08-21 10:46:01 +0530

    Time: 5 ms (30.10%) | Memory: 46.4 MB (70.49%) - LeetSync

--- Arrays/PrefixSum/3034-points-that-intersect-with-cars/points-that-intersect-with-cars.java
+++ Arrays/PrefixSum/3034-points-that-intersect-with-cars/points-that-intersect-with-cars.java
@@ -1,15 +1,26 @@
+// class Solution {
+//     public int numberOfPoints(List<List<Integer>> nums) {
+//         nums.sort((a, b) -> Integer.compare(a.get(0), b.get(0)));
+//         int points=0,le=0;
+//         points=(nums.get(0).get(0))-1;
+//         le=nums.get(0).get(1);
+//         for(int i=1;i<nums.size();i++){
+//             if(nums.get(i).get(0)>le){
+//                 points+=nums.get(i).get(0)-(le+1);
+//             }
+//             if(le<nums.get(i).get(1)) le=nums.get(i).get(1);
+//         }
+//         return le-points;
+//     }
+// }
 class Solution {
     public int numberOfPoints(List<List<Integer>> nums) {
-        nums.sort((a, b) -> Integer.compare(a.get(0), b.get(0)));
-        int points=0,le=0;
-        points=(nums.get(0).get(0))-1;
-        le=nums.get(0).get(1);
-        for(int i=1;i<nums.size();i++){
-            if(nums.get(i).get(0)>le){
-                points+=nums.get(i).get(0)-(le+1);
+        Set<Integer> uniquePoints = new HashSet<>();
+        for (List<Integer> car : nums) {
+            for (int i = car.get(0); i <= car.get(1); i++) {
+                uniquePoints.add(i);
             }
-            if(le<nums.get(i).get(1)) le=nums.get(i).get(1);
         }
-        return le-points;
+        return uniquePoints.size();
     }
 }
