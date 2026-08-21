68d5d06cad041b480caccf4ed509cf8391025208
KUMAR <cjvkumarraja@gmail.com>
2026-08-21 10:40:07 +0530

    Time: 5 ms (30.10%) | Memory: 46.6 MB (33.40%) - LeetSync

---
+++ Arrays/PrefixSum/3034-points-that-intersect-with-cars/points-that-intersect-with-cars.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int numberOfPoints(List<List<Integer>> nums) {
+        nums.sort((a, b) -> Integer.compare(a.get(0), b.get(0)));
+        int points=0,le=0;
+        points=(nums.get(0).get(0))-1;
+        le=nums.get(0).get(1);
+        for(int i=1;i<nums.size();i++){
+            if(nums.get(i).get(0)>le){
+                points+=nums.get(i).get(0)-(le+1);
+            }
+            le=Math.max(le,nums.get(i).get(1));
+        }
+        return le-points;
+    }
+}
