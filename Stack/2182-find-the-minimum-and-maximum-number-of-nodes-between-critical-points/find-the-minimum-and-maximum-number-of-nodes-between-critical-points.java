233a3b90133e5eb52a3d57e42cb9160595cc4a81
KUMAR <cjvkumarraja@gmail.com>
2026-09-01 19:08:05 +0530

    Time: 4 ms (99.87%) | Memory: 106.2 MB (55.41%) - LeetSync

--- Stack/2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.java
+++ Stack/2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.java
@@ -1,52 +1,90 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
+// /**
+//  * Definition for singly-linked list.
+//  * public class ListNode {
+//  *     int val;
+//  *     ListNode next;
+//  *     ListNode() {}
+//  *     ListNode(int val) { this.val = val; }
+//  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+//  * }
+//  */
+// class Solution {
+//     public int[] nodesBetweenCriticalPoints(ListNode head) {
+//         ListNode temp = head.next;
+//         ListNode prev= head;
+//         boolean valid=false;
+//         int c=1,minfirstele=-1,maxfirstele=-1,min=Integer.MAX_VALUE,max=-1;
+//         int [] ans=new int[2];
+//         while(temp.next!=null){
+//             c++;
+//             if(temp.val>prev.val && temp.val>temp.next.val){
+//                 valid=true;
+//             }
+//             if(temp.val<prev.val && temp.val<temp.next.val){
+//                 valid=true;
+//             }
+//             // min=Integer.MAX_VALUE;
+//             // int max=-1;
+//             if(valid){
+//                 if(minfirstele<0){
+//                     maxfirstele=c;
+//                 }
+//                 else{
+//                 // max=c-maxfirstele;
+//                 min=Math.min(c-minfirstele,min);
+//                 }
+//                 minfirstele=c;
+//             }
+//             valid=false;
+//             prev = temp;
+//             temp=temp.next;
+//         }
+//         if(min==Integer.MAX_VALUE){
+//             ans[0]=-1;
+//             ans[1]=-1;
+//         }
+//         else {
+//             ans[0]=min;
+//             ans[1]=max;
+//         }
+//         return ans;
+//     }
+// }
 class Solution {
     public int[] nodesBetweenCriticalPoints(ListNode head) {
+        ListNode prev = head;
         ListNode temp = head.next;
-        ListNode prev= head;
-        boolean valid=false;
-        int c=1,minfirstele=-1,maxfirstele=-1,min=Integer.MAX_VALUE,max=-1;
-        int [] ans=new int[2];
-        while(temp.next!=null){
-            c++;
-            if(temp.val>prev.val && temp.val>temp.next.val){
-                valid=true;
-            }
-            if(temp.val<prev.val && temp.val<temp.next.val){
-                valid=true;
-            }
-            // min=Integer.MAX_VALUE;
-            // int max=-1;
-            if(valid){
-                if(minfirstele<0){
-                    maxfirstele=c;
+        
+        int c = 1; 
+        int firstCrit = -1;
+        int prevCrit = -1;
+        int min = Integer.MAX_VALUE;
+        
+        while (temp.next != null) {
+            // Check for local minimum OR local maximum in one clean statement
+            if ((temp.val > prev.val && temp.val > temp.next.val) || 
+                (temp.val < prev.val && temp.val < temp.next.val)) {
+                
+                if (firstCrit == -1) {
+                    firstCrit = c; // Mark the very first critical point
+                } else {
+                    // Min distance is strictly between adjacent critical points
+                    min = Math.min(min, c - prevCrit);
                 }
-                else{
-                max=c-maxfirstele;
-                min=Math.min(c-minfirstele,min);
-                }
-                minfirstele=c;
+                prevCrit = c; // Update the previous critical point to current
             }
-            valid=false;
+            
+            // FIX: You forgot to move the 'prev' pointer forward!
             prev = temp;
-            temp=temp.next;
+            temp = temp.next;
+            c++;
         }
-        if(min==Integer.MAX_VALUE){
-            ans[0]=-1;
-            ans[1]=-1;
+        
+        if (min == Integer.MAX_VALUE) {
+            return new int[]{-1, -1};
         }
-        else {
-            ans[0]=min;
-            ans[1]=max;
-        }
-        return ans;
+        
+        // FIX: Max distance is ALWAYS the very last critical point minus the first
+        return new int[]{min, prevCrit - firstCrit};
     }
 }
