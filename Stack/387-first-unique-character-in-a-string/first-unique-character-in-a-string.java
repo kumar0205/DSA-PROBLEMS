f02d38c9f6f5fd0ee76b0908f96bbacc9e3dbce0
KUMAR <cjvkumarraja@gmail.com>
2026-09-02 18:12:43 +0530

    Time: 5 ms (98.17%) | Memory: 47.1 MB (39.99%) - LeetSync

--- Stack/387-first-unique-character-in-a-string/first-unique-character-in-a-string.java
+++ Stack/387-first-unique-character-in-a-string/first-unique-character-in-a-string.java
@@ -4,28 +4,25 @@ class Solution {
         // HashMap<Character,Integer> hm  = new HashMap<>();
         int [] freq=new int[26];
         for(int i=0;i<s.length();i++){
-            char c= s.charAt(i);
             // if(!hm.containsKey(c)){
             //     hm.put(c,1);
             //     q.offer(c);
             // }
-            freq[c-'a']++;
+            freq[s.charAt(i)-'a']++;
             // else{
             //     hm.put(c,hm.get(c)+1);
             // }
         }
-        int ans=-1;
+        // int ans=-1;
         for(int i=0;i<s.length();i++){
-            char ch = s.charAt(i);
             // if(hm.get(ch)==1){
             //     ans=s.indexOf(ch);
             //     break;
             // }
-            if(freq[ch-'a']==1){
-                ans=s.indexOf(ch);
-                break;
+            if(freq[s.charAt(i)-'a']==1){
+                return i;
             }
         }
-        return ans;
+        return -1;
     }
 }
