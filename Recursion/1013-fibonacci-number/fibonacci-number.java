04978dd1baba19c84a90892d2f726e0bcf62bc5b
KUMAR <cjvkumarraja@gmail.com>
2026-09-07 19:44:45 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (70.28%) - LeetSync

--- Recursion/1013-fibonacci-number/fibonacci-number.java
+++ Recursion/1013-fibonacci-number/fibonacci-number.java
@@ -2,6 +2,11 @@ class Solution {
     public int fib(int n) {
         if(n==0) return 0;
         if(n==1) return 1;
-        return fib(n-1) + fib(n-2);
+        return fib1(n,0,1);
+    }
+    public int fib1(int n,int a,int b){
+        // if(n==0) return 0;
+        if(n==1) return b;
+        return fib1(n-1,b,a+b);
     }
 }
